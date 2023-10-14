56ba23066899658447fcc294c6c9ab92e9cd5919
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-10-14 16:32:33 +0300

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from selene.support.shared import browser
+from selenium import webdriver
 
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
 
-@pytest.fixture(scope='session', autouse=True)
-def browser_size():
+    browser.config.driver_options = options
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    yield
+    browser.config.timeout = 15
